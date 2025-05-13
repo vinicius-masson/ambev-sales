a4159b3c12797faf9c4c278aa1751c821156fca4
Vinicius <viniciusmasson0@gmail.com>
2025-05-13 10:05:08 -0400

    feat(persistence): add EF Core configurations and migration for Product and Sale entities

---
+++ template/backend/src/Ambev.DeveloperEvaluation.ORM/Mapping/ProductConfiguration.cs
@@ -0,0 +1,27 @@
+﻿using Ambev.DeveloperEvaluation.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Ambev.DeveloperEvaluation.ORM.Mapping
+{
+    public class ProductConfiguration : IEntityTypeConfiguration<Product>
+    {
+        public void Configure(EntityTypeBuilder<Product> builder)
+        {
+            builder.ToTable("Products");
+
+            builder.HasKey(p => p.Id);
+            builder.Property(p => p.Id).HasColumnType("uuid").HasDefaultValueSql("gen_random_uuid()");
+
+            builder.Property(u => u.Name).IsRequired().HasMaxLength(30);
+            builder.Property(p => p.Quantity).IsRequired();
+            builder.Property(p => p.UnitPrice).IsRequired().HasColumnType("decimal(18,2)");
+            builder.Property(p => p.Discount).HasColumnType("decimal(18,2)");
+            builder.Property(p => p.TotalPrice).HasColumnType("decimal(18,2)");
+
+            builder.Property(p => p.Status)
+                .HasConversion<string>()
+                .HasMaxLength(20);
+        }
+    }
+}
---
+++ template/backend/src/Ambev.DeveloperEvaluation.ORM/Mapping/SaleConfiguration.cs
@@ -0,0 +1,35 @@
+﻿using Ambev.DeveloperEvaluation.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Ambev.DeveloperEvaluation.ORM.Mapping
+{
+    public class SaleConfiguration : IEntityTypeConfiguration<Sale>
+    {
+        public void Configure(EntityTypeBuilder<Sale> builder)
+        {
+            builder.ToTable("Sales");
+
+            builder.HasKey(s => s.Id);
+            builder.Property(s => s.Id).HasColumnType("uuid").HasDefaultValueSql("gen_random_uuid()");
+
+            builder.Property(s => s.Number).IsRequired();
+
+            builder.Property(s => s.SaleDate).IsRequired();
+
+            builder.Property(s => s.Customer).IsRequired().HasMaxLength(30);
+
+            builder.Property(s => s.Branch).IsRequired().HasMaxLength(30);
+
+            builder.Property(s => s.TotalSaleAmount).HasColumnType("decimal(18,2)");
+
+            builder.Property(s => s.Status).IsRequired().HasConversion<string>().HasMaxLength(20);
+
+            builder.HasMany(s => s.Products)
+                .WithOne()
+                .HasForeignKey("SaleId")
+                .IsRequired(true);
+
+        }
+    }
+}
