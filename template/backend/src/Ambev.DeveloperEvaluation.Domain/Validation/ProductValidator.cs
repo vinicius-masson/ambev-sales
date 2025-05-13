f826340e67f40e5a512ea0070fc65c557ecfaa9a
Vinicius <viniciusmasson0@gmail.com>
2025-05-13 09:42:08 -0400

    feat(validation): add FluentValidation validators for Sale and Product

---
+++ template/backend/src/Ambev.DeveloperEvaluation.Domain/Validation/ProductValidator.cs
@@ -0,0 +1,23 @@
+﻿using Ambev.DeveloperEvaluation.Domain.Entities;
+using Ambev.DeveloperEvaluation.Domain.Enums;
+using FluentValidation;
+
+namespace Ambev.DeveloperEvaluation.Domain.Validation
+{
+    public class ProductValidator : AbstractValidator<Product>
+    {
+        public ProductValidator()
+        {
+            RuleFor(product => product.Name)
+                .NotEmpty()
+                .MinimumLength(5).WithMessage("Product must be at least 5 characters long.")
+                .MaximumLength(30).WithMessage("Product cannot be longer than 30 characters.");
+
+            RuleFor(product => product.Quantity)
+                .GreaterThan(0)
+                .LessThanOrEqualTo(20).WithMessage("Quantity cannot exceed 20.");
+
+            RuleFor(product => product.UnitPrice).GreaterThan(0);
+        }
+    }
+}
---
+++ template/backend/src/Ambev.DeveloperEvaluation.Domain/Validation/SaleValidator.cs
@@ -0,0 +1,25 @@
+﻿using Ambev.DeveloperEvaluation.Domain.Entities;
+using FluentValidation;
+
+namespace Ambev.DeveloperEvaluation.Domain.Validation
+{
+    public class SaleValidator : AbstractValidator<Sale>
+    {
+        public SaleValidator()
+        {
+            RuleFor(sale => sale.Customer)
+                .NotEmpty()
+                .MinimumLength(5).WithMessage("Customer must be at least 5 characters long.")
+                .MaximumLength(30).WithMessage("Customer cannot be longer than 30 characters.");
+
+
+            RuleFor(sale => sale.Branch)
+                .NotEmpty()
+                .MinimumLength(5).WithMessage("Branch must be at least 5 characters long.")
+                .MaximumLength(30).WithMessage("Branch cannot be longer than 30 characters.");
+
+            RuleFor(sale => sale.Products)
+                .NotEmpty().WithMessage("Sale must have at least one product.");
+        }
+    }
+}
