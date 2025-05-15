914b5861409ad682b86c674d5f16ec46f1e3030b
Vinicius <viniciusmasson0@gmail.com>
2025-05-15 19:27:55 -0400

    test(aplication): add CreateSaleHandlerTests

---
+++ template/backend/tests/Ambev.DeveloperEvaluation.Unit/Application/CreateSaleHandlerTests.cs
@@ -0,0 +1,99 @@
+﻿using Ambev.DeveloperEvaluation.Application.Sales.CreateSale;
+using Ambev.DeveloperEvaluation.Application.Users.CreateUser;
+using Ambev.DeveloperEvaluation.Domain.Entities;
+using Ambev.DeveloperEvaluation.Domain.Repositories;
+using Ambev.DeveloperEvaluation.ORM.Repositories;
+using Ambev.DeveloperEvaluation.Unit.Application.TestData;
+using Ambev.DeveloperEvaluation.Unit.Domain;
+using AutoMapper;
+using FluentAssertions;
+using NSubstitute;
+using Xunit;
+
+namespace Ambev.DeveloperEvaluation.Unit.Application
+{
+    /// <summary>
+    /// Contains unit tests for the <see cref="CreateSaleHandler"/> class.
+    /// </summary>
+    public class CreateSaleHandlerTests
+    {
+        private readonly ISaleRepository _saleRepository;
+        private readonly IMapper _mapper;
+        private readonly CreateSaleHandler _handler;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="CreateSaleHandlerTests"/> class.
+        /// Sets up the test dependencies and creates fake data generators.
+        /// </summary>
+        public CreateSaleHandlerTests()
+        {
+            _saleRepository = Substitute.For<ISaleRepository>();
+            _mapper = Substitute.For<IMapper>();
+            _handler = new CreateSaleHandler(_saleRepository, _mapper);
+        }
+
+        /// <summary>
+        /// Tests that a valid sale creation request is handled successfully.
+        /// </summary>
+        [Fact(DisplayName = "Given valid sale data When creating sale Then returns success response")]
+        public async Task Handle_ValidRequest_ReturnsSuccessResponse()
+        {
+            // Given
+            var command = CreateSaleHandlerTestData.GenerateValidCommand();
+            var sale = new Sale(command.Customer, command.Branch);
+            sale.Id = Guid.NewGuid();
+
+            var result = new CreateSaleResult
+            {
+                Id = sale.Id
+            };
+
+
+            _mapper.Map<Sale>(command).Returns(sale);
+            _mapper.Map<CreateSaleResult>(sale).Returns(result);
+
+            _saleRepository.CreateAsync(Arg.Any<Sale>(), Arg.Any<CancellationToken>())
+                .Returns(sale);
+
+            // When
+            var createSaleResult = await _handler.Handle(command, CancellationToken.None);
+
+            // Then
+            createSaleResult.Should().NotBeNull();
+            createSaleResult.Id.Should().Be(sale.Id);
+            await _saleRepository.Received(1).CreateAsync(Arg.Any<Sale>(), Arg.Any<CancellationToken>());
+        }
+
+        /// <summary>
+        /// Tests that an invalid sale creation request throws a validation exception.
+        /// </summary>
+        [Fact(DisplayName = "Given invalid sale data When creating sale Then throws validation exception")]
+        public async Task Handle_InvalidRequest_ThrowsValidationException()
+        {
+            // Given
+            var command = new CreateSaleCommand();
+
+            // When
+            var act = () => _handler.Handle(command, CancellationToken.None);
+
+            // Then
+            await act.Should().ThrowAsync<FluentValidation.ValidationException>();
+        }
+
+        /// <summary>
+        /// Tests that a sale with no products
+        /// </summary>
+        [Fact(DisplayName = "Given a sale without products When handling Then throws validation exception")]
+        public async Task Handle_ValidRequest_MapsCommandToUser()
+        {
+            // Given
+            var command = CreateSaleHandlerTestData.GenerateSaleCommandWithNoProducts();
+
+            // When
+            var act = () => _handler.Handle(command, CancellationToken.None);
+
+            // Then
+            await act.Should().ThrowAsync<FluentValidation.ValidationException>();
+        }
+    }
+}
