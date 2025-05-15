557370bb9265a4b7d1bc29ca3d27878e1c6f2750
Vinicius <viniciusmasson0@gmail.com>
2025-05-15 19:24:04 -0400

    feat(aplication): add DeleteSale command, handler, validator and response

---
+++ template/backend/src/Ambev.DeveloperEvaluation.Application/Sales/DeleteSale/DeleteSaleHandler.cs
@@ -0,0 +1,35 @@
+﻿using Ambev.DeveloperEvaluation.Domain.Repositories;
+using AutoMapper;
+using FluentValidation;
+using MediatR;
+
+namespace Ambev.DeveloperEvaluation.Application.Sales.DeleteSale
+{
+    public class DeleteSaleHandler : IRequestHandler<DeleteSaleCommand, DeleteSaleResponse>
+    {
+        private readonly ISaleRepository _saleRepository;
+        private readonly IMapper _mapper;
+
+        public DeleteSaleHandler(ISaleRepository saleRepository, IMapper mapper)
+        {
+            _mapper = mapper;
+            _saleRepository = saleRepository;
+        }
+
+        public async Task<DeleteSaleResponse> Handle(DeleteSaleCommand request, CancellationToken cancellationToken)
+        {
+            var validator = new DeleteSaleCommandValidator();
+            var validationResult = await validator.ValidateAsync(request, cancellationToken);
+
+            if (!validationResult.IsValid)
+                throw new ValidationException(validationResult.Errors);
+
+            var success = await _saleRepository.DeleteAsync(request.Id);
+
+            if (!success)
+                throw new KeyNotFoundException($"Sale with ID {request.Id} not found");
+
+            return new DeleteSaleResponse { Success = true };
+        }
+    }
+}
---
+++ template/backend/src/Ambev.DeveloperEvaluation.Application/Sales/DeleteSale/DeleteSaleValidator.cs
@@ -0,0 +1,14 @@
+﻿using FluentValidation;
+
+namespace Ambev.DeveloperEvaluation.Application.Sales.DeleteSale
+{
+    public class DeleteSaleCommandValidator : AbstractValidator<DeleteSaleCommand>
+    {
+        public DeleteSaleCommandValidator()
+        {
+            RuleFor(s => s.Id)
+                .NotEmpty()
+                .WithMessage("Sale Id is Required");
+        }
+    }
+}
